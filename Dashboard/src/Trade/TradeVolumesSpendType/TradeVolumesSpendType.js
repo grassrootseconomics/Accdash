885fe752b7cecb48f6ef5a6dc2d113b779bde972
chakrakanakamedala <chakra.kanakamedala@accenture.com>
2020-04-08 14:09:38 +0100

    Folder structure change

---
+++ Dashboard/src/Trade/TradeVolumesSpendType/TradeVolumesSpendType.js
@@ -0,0 +1,60 @@
+import React from "react";
+import HBarChart from "../../Components/HBarChart/HBarChart";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+import "./TradeVolumesSpendType.scss";
+
+const summary = gql(`
+query Summary($from:String!, $to:String!, $tokens:[String]!, $spendTypes:[String]!, $gender:[String]!, $txType:[String]!){  
+  categorySummary (
+    fromDate:$from, toDate:$to,  tokenName:$tokens, spendType:$spendTypes, gender:$gender, txType:$txType, request:"tradevolumes-category-spendtype"
+    ){
+      label
+      value
+    }
+}
+`);
+
+export default class TradeVolumesSpendType extends React.Component {
+  render() {
+    return (
+      <section id="tradeVolumesSpendType">
+        <p className="title">TRADE VOLUMES BY SPEND TYPE</p>
+        <Query
+          query={summary}
+          variables={{
+            from: this.props.from,
+            to: this.props.to,
+            tokens: this.props.tokens,
+            spendTypes: this.props.spendTypes,
+            gender: this.props.gender,
+            txType: this.props.txType
+          }}
+        >
+          {({ loading, error, data }) => {
+            if (loading) {
+              return <p>Loading data...</p>;
+            } else if (error) {
+              return <p>API returned an error Please try again</p>;
+            } else {
+              const chartData = data.categorySummary;
+              if (chartData.length > 0) {
+                return (
+                  <HBarChart
+                    title={"Trade Volumes Bar"}
+                    keys={this.props.spendTypes}
+                    data={data.categorySummary}
+                    width={250}
+                    height={250}
+                  />
+                );
+              }
+              return <p>There is no data for the current selection</p>;
+            }
+          }}
+        </Query>
+      </section>
+    );
+  }
+}
---
+++ Dashboard/src/Trades/TradeVolumes/TradeVolumes.js
@@ -0,0 +1,89 @@
+import React from "react";
+import StackedBarChart from "../../Components/StackedBarChart/StackedBarChart";
+import LineChart from "../../Components/LineChart/LineChart";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import "./TradeVolumes.scss";
+
+const summary = gql(`
+query MonthlySummary($from:String!, $to:String!, $tokens:[String]!, $spendTypes:[String]!, $gender:[String]!, $txType:[String]!, $tradeType:String!){
+  monthlySummaryData  (fromDate:$from, toDate:$to,  tokenName:$tokens, spendType:$spendTypes, gender:$gender, txType:$txType, , request:$tradeType){ 
+    value
+  }
+}
+`);
+
+export default class TradeVolumes extends React.Component {
+  render() {
+    return (
+      <section id="tradeVolumes">
+        <p className="title">TRADE VOLUMES</p>
+        <Query
+          query={summary}
+          variables={{
+            from: this.props.from,
+            to: this.props.to,
+            tokens: this.props.tokens,
+            spendTypes: this.props.spendTypes,
+            gender: this.props.gender,
+            txType: this.props.txType,
+            tradeType: `tradevolumes-time-${this.props.tradeType}`
+          }}
+        >
+          {({ loading, error, data }) => {
+            const colors =
+              this.props.tradeType === "gender"
+                ? ["#3b5998", "#8b9dc3", "#536878", "#4279a3"]
+                : [
+                    "#38DCE2",
+                    "#32AF93",
+                    "#248890",
+                    "#74D485",
+                    "#68EEAB",
+                    "#CAF270",
+                    "#2FADB6",
+                    "#66FCF1",
+                    "#1A505B",
+                    "#4472C4",
+                    "#1B2A37",
+                    "#8EBFF2"
+                  ];
+            if (loading) {
+              return <p>Loading data...</p>;
+            } else if (error) {
+              return <p>API returned an error Please try again</p>;
+            } else {
+              const chartData = data.monthlySummaryData[0].value;
+              if (chartData.length > 0) {
+                return this.props.tradeType === "spendtype" ? (
+                  <StackedBarChart
+                    title={"Trade Volumes"}
+                    data={chartData}
+                    keys={Object.keys(chartData[0]).slice(1)}
+                    width={900}
+                    height={275}
+                    startMonth={this.props.from}
+                    endMonth={this.props.to}
+                    colors={colors}
+                  />
+                ) : (
+                  <LineChart
+                    title={"Trade Volumes"}
+                    data={chartData}
+                    keys={Object.keys(chartData[0]).slice(1)}
+                    width={900}
+                    height={275}
+                    startMonth={this.props.from}
+                    endMonth={this.props.to}
+                    colors={colors}
+                  />
+                );
+              }
+              return <p>There is no data for the current selection</p>;
+            }
+          }}
+        </Query>
+      </section>
+    );
+  }
+}
