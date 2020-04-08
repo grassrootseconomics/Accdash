e4482c0090188a2b11e81b535d4ef596d353ae43
chakrakanakamedala <chakra.kanakamedala@accenture.com>
2020-04-08 16:52:44 +0100

    graphQl config changes

--- Dashboard/src/App/App.js
+++ Dashboard/src/App/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
-import Layout from "../../Layout/Layout";
+import Layout from "../Layout/Layoutyout";
 
 class App extends React.Component {
   render() {
