88c032db1391154f47ee43655974ee728c514cb5
chakrakanakamedala <chakra.kanakamedala@accenture.com>
2020-04-08 14:02:38 +0100

    Code restructuring

--- Dashboard/src/Layout/Layout.js
+++ Dashboard/src/Layout/Layout.js
@@ -4,12 +4,12 @@ import Sidebar from ".././Components/Sidebar/Sidebar";
 import Backdrop from "../Backdrop/Backdrop";
 import UsersSummary from "../Summary/UsersSummary";
 import TradeSummary from "../Summary/TradeSummary";
-import Users from "../Components/Users/Users";
-import TradeVolumes from "../Components/TradeVolumes/TradeVolumes";
-import Traders from "../Components/TradeVolumes/Traders";
-import TradeVolumesGender from "../Components/TradeVolumes/TradeVolumesGender";
-import Transactions from "../Components/Transactions/Transactions";
-import TradeVolumesSpendType from "../Components/TradeVolumes/TradeVolumesSpendType";
+import Users from "../Users/Users";
+import TradeVolumes from "../Trades/TradeVolumes/TradeVolumes";
+import Traders from "../Trades/Traders/Traders";
+import TradeVolumesGender from "../Trades/TradeVolumesGender/TradeVolumesGender";
+import Transactions from "../Trades/Transactions/Transactions";
+import TradeVolumesSpendType from "../Trades/TradeVolumesSpendType/TradeVolumesSpendType";
 import { Row, Col, Container } from "react-bootstrap";
 import "./Layout.scss";
 
--- Dashboard/src/utilities/GraphQLConfig.js
+++ Dashboard/src/utilities/GraphQLConfig.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
+import React from "./node_modules/react";
+import ApolloClient from "./node_modules/apollo-boost";
+import { ApolloProvider } from "./node_modules/react-apollo";
 
 const client = new ApolloClient({
   uri: "http://127.0.0.1:8080/graphql"
