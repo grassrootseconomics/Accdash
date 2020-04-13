fb095455b94eca8a71a568cc45f2f1f37012831a
Vaneet <vaneet.gupta@accenture.com>
2020-04-13 09:08:39 +0530

    Added environment variables

--- Dashboard/src/index.js
+++ Dashboard/src/index.js
@@ -9,7 +9,7 @@ import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
 const httpLink = createHttpLink({
-  uri: "https://iqr3ivy96j.execute-api.eu-central-1.amazonaws.com/prod/graphql/"
+  uri: "$api_gateway"
 });
 
 const client = new ApolloClient({
